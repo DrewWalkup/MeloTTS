dbb862030392014b2d3538a91abd2b163792ad3a
DrewWalkup <drew.walkup@gmail.com>
2024-07-31 23:21:25 -0700

    add ability to select cuda device on multi-gpu system

--- melo/api.py
+++ melo/api.py
@@ -17,32 +17,29 @@ from .split_utils import split_sentence
 from .mel_processing import spectrogram_torch, spectrogram_torch_conv
 from .download_utils import load_or_download_config, load_or_download_model
 
-
 class TTS(nn.Module):
-    def __init__(
-        self,
-        language,
-        device="auto",
-        use_hf=True,
-        config_path=None,
-        ckpt_path=None,
-        cuda_device=None,
-    ):
+    def __init__(self, 
+                language,
+                device='auto',
+                use_hf=True,
+                config_path=None,
+                ckpt_path=None,
+                cuda_device=None):
         super().__init__()
-        if device == "auto":
-            device = "cpu"
-            if cuda_device is not None:
-                device = f"cuda:{cuda_device}"
-            else:
-                device = "cuda"
-            if torch.backends.mps.is_available():
-                device = "mps"
-        if "cuda" in device:
+        if device == 'auto':
+            device = 'cpu'
+            if torch.cuda.is_available():
+                if cuda_device is not None:
+                    device = f'cuda:{cuda_device}'
+                else:
+                    device = 'cuda'
+            if torch.backends.mps.is_available(): device = 'mps'
+        if 'cuda' in device:
             assert torch.cuda.is_available()
             if cuda_device is not None:
                 torch.cuda.set_device(cuda_device)
 
-        # config_path =
+        # config_path = 
         hps = load_or_download_config(language, use_hf=use_hf, config_path=config_path)
 
         num_languages = hps.num_languages
@@ -64,20 +61,16 @@ class TTS(nn.Module):
         self.symbol_to_id = {s: i for i, s in enumerate(symbols)}
         self.hps = hps
         self.device = device
-
+    
         # load state_dict
-        checkpoint_dict = load_or_download_model(
-            language, device, use_hf=use_hf, ckpt_path=ckpt_path
-        )
-        self.model.load_state_dict(checkpoint_dict["model"], strict=True)
-
-        language = language.split("_")[0]
-        self.language = (
-            "ZH_MIX_EN" if language == "ZH" else language
-        )  # we support a ZH_MIX_EN model
+        checkpoint_dict = load_or_download_model(language, device, use_hf=use_hf, ckpt_path=ckpt_path)
+        self.model.load_state_dict(checkpoint_dict['model'], strict=True)
+        
+        language = language.split('_')[0]
+        self.language = 'ZH_MIX_EN' if language == 'ZH' else language # we support a ZH_MIX_EN model
 
     @staticmethod
-    def audio_numpy_concat(segment_data_list, sr, speed=1.0):
+    def audio_numpy_concat(segment_data_list, sr, speed=1.):
         audio_segments = []
         for segment_data in segment_data_list:
             audio_segments += segment_data.reshape(-1).tolist()
@@ -90,24 +83,11 @@ class TTS(nn.Module):
         texts = split_sentence(text, language_str=language)
         if not quiet:
             print(" > Text split to sentences.")
-            print("\n".join(texts))
+            print('\n'.join(texts))
             print(" > ===========================")
         return texts
 
-    def tts_to_file(
-        self,
-        text,
-        speaker_id,
-        output_path=None,
-        sdp_ratio=0.2,
-        noise_scale=0.6,
-        noise_scale_w=0.8,
-        speed=1.0,
-        pbar=None,
-        format=None,
-        position=None,
-        quiet=False,
-    ):
+    def tts_to_file(self, text, speaker_id, output_path=None, sdp_ratio=0.2, noise_scale=0.6, noise_scale_w=0.8, speed=1.0, pbar=None, format=None, position=None, quiet=False,):
         language = self.language
         texts = self.split_sentences_into_pieces(text, language, quiet)
         audio_list = []
@@ -121,12 +101,10 @@ class TTS(nn.Module):
             else:
                 tx = tqdm(texts)
         for t in tx:
-            if language in ["EN", "ZH_MIX_EN"]:
-                t = re.sub(r"([a-z])([A-Z])", r"\1 \2", t)
+            if language in ['EN', 'ZH_MIX_EN']:
+                t = re.sub(r'([a-z])([A-Z])', r'\1 \2', t)
             device = self.device
-            bert, ja_bert, phones, tones, lang_ids = utils.get_text_for_tts_infer(
-                t, language, self.hps, device, self.symbol_to_id
-            )
+            bert, ja_bert, phones, tones, lang_ids = utils.get_text_for_tts_infer(t, language, self.hps, device, self.symbol_to_id)
             with torch.no_grad():
                 x_tst = phones.to(device).unsqueeze(0)
                 tones = tones.to(device).unsqueeze(0)
@@ -136,8 +114,7 @@ class TTS(nn.Module):
                 x_tst_lengths = torch.LongTensor([phones.size(0)]).to(device)
                 del phones
                 speakers = torch.LongTensor([speaker_id]).to(device)
-                audio = (
-                    self.model.infer(
+                audio = self.model.infer(
                         x_tst,
                         x_tst_lengths,
                         speakers,
@@ -148,26 +125,18 @@ class TTS(nn.Module):
                         sdp_ratio=sdp_ratio,
                         noise_scale=noise_scale,
                         noise_scale_w=noise_scale_w,
-                        length_scale=1.0 / speed,
-                    )[0][0, 0]
-                    .data.cpu()
-                    .float()
-                    .numpy()
-                )
+                        length_scale=1. / speed,
+                    )[0][0, 0].data.cpu().float().numpy()
                 del x_tst, tones, lang_ids, bert, ja_bert, x_tst_lengths, speakers
-                #
+                # 
             audio_list.append(audio)
         torch.cuda.empty_cache()
-        audio = self.audio_numpy_concat(
-            audio_list, sr=self.hps.data.sampling_rate, speed=speed
-        )
+        audio = self.audio_numpy_concat(audio_list, sr=self.hps.data.sampling_rate, speed=speed)
 
         if output_path is None:
             return audio
         else:
             if format:
-                soundfile.write(
-                    output_path, audio, self.hps.data.sampling_rate, format=format
-                )
+                soundfile.write(output_path, audio, self.hps.data.sampling_rate, format=format)
             else:
                 soundfile.write(output_path, audio, self.hps.data.sampling_rate)
